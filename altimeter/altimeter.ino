9e229df2bcf348b9c9df27afa79faa98c158519b
tharnyelilvannan <tharny2005@outlook.com>
2024-10-10 09:06:46 -0400

    initial commit

---
+++ altimeter/altimeter.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_BMP085.h>
+
+/*
+ * Author: Tharny Elilvannan
+ * Last Updated: October 10, 2024
+ * Purpose: Gives user the altittude.
+ */
+
+// initiate barometric pressure sensor 
+Adafruit_BMP085 bmps;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
