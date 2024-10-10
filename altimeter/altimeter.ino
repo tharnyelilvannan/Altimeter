055fd92471b512395bb4fb8d92d2d692b9271839
tharnyelilvannan <tharny2005@outlook.com>
2024-10-10 11:20:04 -0400

    prototype code

--- altimeter/altimeter.ino
+++ altimeter/altimeter.ino
@@ -4,12 +4,14 @@
 /*
  * Author: Tharny Elilvannan
  * Last Updated: October 10, 2024
- * Purpose: Gives user the altittude.
+ * Purpose: Gives user the altittude, measured every 10 seconds.
  */
 
 // initiate barometric pressure sensor 
 Adafruit_BMP085 bmps;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+float seaLevelPressure = 102340;
 
 // setup runs once
 void setup() {
@@ -21,26 +23,26 @@ void setup() {
 
   lcd.print("Initializing...");
 
-  delay(1000);
+  delay(5000);
+
+  lcd.clear();
 
   if (bmps.begin() != true) {
-    lcd.println("Error. Check wiring.");
+    lcd.println("Error.");
   } // end of if statement
-
-  while (1) {
-
-  } // end of while
 
 } // end of setup function
 
 // loop runs repeatedly
 void loop() {
   
-  double seaLevelPressure = bmps.readSealevelPressure();
+  lcd.setCursor(0, 0);
 
-  lcd.print("Altitude = ");
-  lcd.print(bmps.readAltitude(seaLevelPressure * 100));
-  lcd.println(" m");
-  delay(1000);
+  lcd.print("Altitude: ");
+  lcd.setCursor(0, 1);
+  lcd.print(bmps.readAltitude(seaLevelPressure));
+  lcd.println(" meters    ");
+  delay(10000);
+  lcd.clear();
 
 } // end of loop function
